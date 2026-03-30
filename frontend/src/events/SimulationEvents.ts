0320c9df1597a4340a4aca8e93c0db10b1e2ff1f
danieljonesOU <daniel.r.jones-1@ou.edu>
2026-03-29 22:36:32 -0500

    refactor(events): add BODY_COLLISION to SimulationEvents

--- frontend/src/events/SimulationEvents.ts
+++ frontend/src/events/SimulationEvents.ts
@@ -2,6 +2,13 @@ export enum SimulationEventType {
     BODY_POSITION_UPDATED = 'sim:position_updated',
     BODY_TRIGGER_FIRED = 'sim:trigger_fired',
     BODIES_LOADED = 'sim:bodies_loaded',
+    BODY_COLLISION = 'sim:collision',
+}
+
+export interface BodyCollisionEvent {
+    idA: string;
+    idB: string;
+    distance: number;
 }
 
 export interface BodyPositionUpdatedEvent {
